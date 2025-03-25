1a9ed030e2ffe720f3ad1da3569203c51be77a45
SSAKTHIVELMURUGAN <s.sakthivelmurugan2003@gmail.com>
2025-03-25 16:02:28 +0530

    feat: Added workflow for scheduled entry

--- scholarship_management/hooks.py
+++ scholarship_management/hooks.py
@@ -87,7 +87,7 @@ doctype_js = {"Payment Entry" : "public/js/payment_entry.js"}
 
 # before_install = "scholarship_management.install.before_install"
 after_install = "scholarship_management.install.after_install"
-# after_migrate = "scholarship_management.install.setup_scholarship_roles"
+# after_migrate = "scholarship_management.install.create_workflow"
 # Uninstallation
 # ------------
 
--- scholarship_management/install.py
+++ scholarship_management/install.py
@@ -5,6 +5,7 @@ def after_install():
 	frappe.get_attr("scholarship_management.scholarship_management.doctype.student.student.party_setup")()
 	frappe.get_attr("scholarship_management.customization.address.address_customization.customize_address_doctype")()
 	setup_scholarship_roles()
+	create_workflow()
 
 def setup_scholarship_roles():
 	roles = ["Scholarship Manager", "Scholarship User"]
@@ -67,3 +68,72 @@ def add_and_update_permissions(doctype, role, permissions):
 			ptype=perm_type,
 			value=value
 		)
+
+
+def create_workflow():
+    states = [
+        "Pending",
+        "Approved",
+        "Rejected"
+    ]
+
+    # Create workflow states if they don't exist
+    for state in states:
+        if not frappe.db.exists("Workflow State", state):
+            workflow_state = frappe.get_doc(
+                {
+                    "doctype": "Workflow State",
+                    "workflow_state_name": state,
+                    "style": "Success"
+                    if state == "Transaction Completed"
+                    else "Danger",
+                }
+            )
+            workflow_state.insert()
+
+    # Create workflow if it doesn't exist
+    if not frappe.db.exists("Workflow", "Scheduled Entry Workflow"):
+        workflow = frappe.get_doc(
+            {
+                "doctype": "Workflow",
+                "workflow_name": "Scheduled Entry Workflow",
+                "document_type": "Scheduled Entry",
+                "is_active": 1,
+                "send_email_alert": 0,
+                "states": [
+                    {
+                        "state": "Pending",
+                        "allow_edit": "Scholarship User",
+                        "doc_status": 0,
+                    },
+                    {
+                        "state": "Approved",
+                        "allow_edit": "Scholarship Manager",
+                        "doc_status": 1,
+                    },
+                    {
+                        "state": "Rejected",
+                        "allow_edit": "Scholarship Manager",
+                        "doc_status": 1,
+                    },
+                ],
+                "transitions": [
+                    {
+                        "state": "Pending",
+                        "action": "Reject",
+                        "next_state": "Rejected",
+                        "allowed": "Scholarship Manager",
+                        "allow_self_approval": 1,
+                    },
+                    {
+                        "state": "Pending",
+                        "action": "Approve",
+                        "next_state": "Approved",
+                        "allowed": "Scholarship Manager",
+                        "allow_self_approval": 1,
+                    },
+                ],
+            }
+        )
+        workflow.insert()
+        frappe.db.commit()
